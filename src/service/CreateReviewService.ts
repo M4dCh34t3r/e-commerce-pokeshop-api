4fdc9088f826551da36bcd8ea766493e5d2277e6
M4dCh34t3r <LegendPotty@gmail.com>
2022-07-26 22:06:49 -0300

    chore: add Review services

---
+++ src/service/CreateReviewService.ts
@@ -0,0 +1,25 @@
+import { ReviewRepository } from "../repository/ReviewRepository";
+import { getCustomRepository } from "typeorm";
+
+interface ReviewProps {
+  author: string;
+  message: string
+}
+
+export class CreateReviewService {
+  async execute({ author, message }: ReviewProps) {
+    const reviewRepository = getCustomRepository(ReviewRepository);
+
+    if (!author) {
+        throw new Error("Please, insert an author...")
+    }
+
+    if (!message) {
+        throw new Error("Please, insert a message...")
+    }
+
+    const createdReview = reviewRepository.create({ author, message })
+    await reviewRepository.save(createdReview);
+    return createdReview;
+  }
+}
---
+++ src/service/ListReviewService.ts
@@ -0,0 +1,10 @@
+import { ReviewRepository } from "../repository/ReviewRepository";
+import { getCustomRepository } from "typeorm";
+
+export class ListReviewService{
+  async execute() {
+    const reviewRepository = getCustomRepository(ReviewRepository);
+    const allMessages = await reviewRepository.find();
+    return allMessages
+  }
+}
