83a7778459ba39488458c34437c455473340127b
M4dCh34t3r <LegendPotty@gmail.com>
2022-07-26 22:08:14 -0300

    feat: update API routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,14 @@
+import { CreateReviewController } from "./controller/CreateReviewController";
+import { ListReviewController } from "./controller/ListReviewController";
 import { Router, Request, Response } from "express";
 
 export const router = Router();
+
+const createReviewController = new CreateReviewController();
+const listReviewController = new ListReviewController();
+
+router.get('/reviews', listReviewController.handle)
+router.post('/reviews', createReviewController.handle)
 
 router.get('/', (request: Request, response: Response) => {
     return response.json({ message: "The PokeShop API is runninng!" })
