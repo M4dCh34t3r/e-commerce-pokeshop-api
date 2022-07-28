c30839119d0dda7dc084367fc36b46f451522bb3
M4dCh34t3r <LegendPotty@gmail.com>
2022-07-28 13:32:09 -0300

    feat: add 'shop-item' routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,27 @@
+import { CreateShopItemController } from "./controller/CreateShopItemController";
+import { DeleteShopItemController } from "./controller/DeleteShopItemController";
 import { CreateReviewController } from "./controller/CreateReviewController";
+import { ListShopItemController } from "./controller/ListShopitemController";
 import { ListReviewController } from "./controller/ListReviewController";
 import { Router, Request, Response } from "express";
 
 export const router = Router();
 
 const createReviewController = new CreateReviewController();
+const createShopItemController = new CreateShopItemController();
+
+const deleteShopItemController = new DeleteShopItemController();
+
 const listReviewController = new ListReviewController();
+const listShopItemController = new ListShopItemController();
+
+router.delete('/shop-items', deleteShopItemController.handle);
+
+router.post('/reviews', createReviewController.handle);
+router.post('/shop-items', createShopItemController.handle);
 
 router.get('/reviews', listReviewController.handle);
-router.post('/reviews', createReviewController.handle);
+router.get('/shop-items', listShopItemController.handle);
 
 router.get('/', (request: Request, response: Response) => {
     return response.json({ message: "The PokeShop API is runninng!" });
